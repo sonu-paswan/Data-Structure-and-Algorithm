ab32644340b214243157bd2930eaa0b8330f4dd7
Sonu Paswan <sonupas2806@gmail.com>
2021-10-16 16:40:16 +0530

    mdf merge2 program

--- basics/merge2.c
+++ basics/merge2.c
@@ -1,37 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-void merge(int a[],int l,int m,int h){
-    int size=h-l+1;
-    int *array=(int*)malloc(sizeof(int)*size);
-    // int array[size];
-    int i=l,k=0;
-    int j=m+1;
-    while(i<=m&&j<=h){
-        if(a[i]<a[j])
-        array[k++]=a[i++];
+// program to merge in a single array considering two lists
+void merge(int a[], int l, int m, int h)
+{
+    int size = h - l + 1;
+    int i, j, k = 0;
+    int *array = (int *)malloc(sizeof(int) * size);
+    if ((h - l) % 2 == 0)
+    {
+        i = l;
+        j = m;
+    }
+    else
+    {
+        i = l;
+        j = m + 1;
+    }
+    while (i <= m && j <= h)
+    {
+        if (a[i] < a[j])
+            array[k++] = a[i++];
         else
-        array[k++]=a[j++];
+            array[k++] = a[j++];
     }
-    while(i<=m){
-        array[k++]=a[i++];
+    while (i <= m)
+    {
+        array[k++] = a[i++];
     }
-    while(j<=h){
-        array[k++]=a[j++];
+    while (j <= h)
+    {
+        array[k++] = a[j++];
     }
-    for(int i=0;i<size;i++){
-        a[i]=array[i];
+    for (int i = 0; i < size; i++)
+    {
+        a[i] = array[i];
     }
 }
 int main()
 {
-    int arr[]={6,7,8,9,1,2,3,4,5};
-    int len=sizeof(arr)/sizeof(arr[0]);
-    int low=0;
-    int high=7;
-    int mid=(low+high)/2;
-    merge(arr,low,mid,high);
-    for(int i=0;i<len;i++){
-        printf("%d ",arr[i]);
+    int arr[] = {6, 7, 8, 9, 1, 2,3, 4, 5};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int low = 0;
+    int high = len - 1;
+    int mid = (low + high) / 2;
+    merge(arr, low, mid, high);
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d ", arr[i]);
     }
     return 0;
 }
