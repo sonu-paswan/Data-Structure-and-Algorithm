c240f56edd4b97a5c46cf84f79b2d8f648325d92
Sonu Paswan <sonupas2806@gmail.com>
2021-11-09 09:56:54 +0530

    mdf insertion_in_list2.c added address

--- Linked_List/insertion_in_list2.c
+++ Linked_List/insertion_in_list2.c
@@ -24,18 +24,18 @@ void displayValue(struct Node *p)
         p = p->next;
     }
 }
-void insertList(struct Node *head, struct Node *ptr, int data, int position)
+void insertList(struct Node **head, struct Node *ptr, int data, int position)
 {
     ptr->data = data;
     if (position == 0)
     {
 
-        ptr->next = head;
-        head = ptr;
+        ptr->next = *head;
+        *head = ptr;
     }
     else
     {
-        struct Node *temp = head;
+        struct Node *temp = *head;
         for (int i = 0; i < position - 1; i++)
         {
             temp = temp->next;
@@ -58,37 +58,34 @@ int MaxNum(struct Node * p)
 
 int main()
 {
-    struct Node *head = NULL, *second = NULL, *third = NULL, *fourth = NULL;
-    head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *Head = NULL, *second = NULL, *third = NULL, *fourth = NULL;
+    Head = (struct Node *)malloc(sizeof(struct Node));
     second = (struct Node *)malloc(sizeof(struct Node));
     third = (struct Node *)malloc(sizeof(struct Node));
     fourth = (struct Node *)malloc(sizeof(struct Node));
     /////////// setting list ///////////
-    head->next = second;
+    Head->next = second;
     second->next = third;
     third->next = fourth;
     fourth->next = NULL;
     ///////////////////////////////////
     printf("enter four values : ");
-    setValue(head);
+    setValue(Head);
     printf("display values : ");
-    displayValue(head);
+    displayValue(Head);
 
     ////////////// insertion ////////////////
 
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-    insertList(head, newNode, 45, 3);//insert after 3rd element
-    printf("\nafter adding element at fourth poistion\n");
-    displayValue(head);
+    insertList(&Head, newNode, 45, 0);//insert after 3rd element
+    printf("\nafter adding element at zeroth poistion\n");
+    displayValue(Head);
 
-    printf("\ngreatest number is list is %d",MaxNum(head));
+    printf("\ngreatest number is list is %d",MaxNum(Head));
 
     //////////////////
    
-    free(head);
-    free(second);
-    free(third);
-    free(newNode);
-    // free(temp);
+    free(Head);
+    
     return 0;
 }
