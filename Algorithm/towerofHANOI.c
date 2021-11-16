80e5a730cc81bb20fc9a401996d9ed46a9a6bd20
Sonu Paswan <sonupas2806@gmail.com>
2021-11-16 19:04:45 +0530

    mdf tower of hanoi

--- Algorithm/towerofHANOI.c
+++ Algorithm/towerofHANOI.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 //Tower of hanoi problem in c language
 static int move;
+static int call;
 void TOH(int n,int A,int B,int C){
+    call++;
     if(n>0){
         TOH(n-1,A,C,B);
         printf("%c to %c \n",A+64,C+64);
@@ -15,6 +17,9 @@ int main()
     printf("program to solve tower of hanoi follow the steps below\n");
     TOH(n,1,2,3);
     printf("moves taken to solve above problem is %d",move);
+    printf("\nnumber of functions calls %d",call);
     
     return 0;
 }
+// total time taken -> (2^(n+1) - 1)
+// time complexity -> O(2^n)
